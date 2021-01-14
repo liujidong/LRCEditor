af2ba94ce25e634b499c207f5efc6aad2c762b5e
liujidong <wwwliujidong@163.com>
2021-01-14 15:08:52 +0800

    转换maven项目

--- src/main/java/com/plter/lrceditor/App.java
+++ src/main/java/com/plter/lrceditor/App.java
@@ -1,13 +1,15 @@
 package com.plter.lrceditor;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-public class Main extends Application {
-
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/com/plter/lrceditor/views/LRCEditor.fxml"));
